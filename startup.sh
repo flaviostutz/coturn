73691df3df5fdf6e569806300d8f6f3e5e7ecb11
flaviostutz <flaviostutz@gmail.com>
2020-03-14 01:42:27 -0300

    adding port configuration; adding anonymous access authentication

--- startup.sh
+++ startup.sh
@@ -11,5 +11,15 @@ echo "/etc/turnserver.conf"
 envsubst < /tmp/turnserver.conf > /etc/turnserver.conf
 echo /etc/turnserver.conf
 
+if [ "$TURN_USERNAME" == "" ] && [ "$TURN_PASSWORD" == "" ]; then
+    echo "WARNING: Allowing anonymous access to TURN server"
+    sed -i 's/lt-cred-mech/#lt-cred-mech/g' /etc/turnserver.conf
+    sed -i 's/#no-auth/no-auth/g' /etc/turnserver.conf
+fi
+
+echo /etc/turnserver.conf
+cat /etc/turnserver.conf
+echo ""
+
 echo "Starting turnserver..."
 turnserver -v -c /etc/turnserver.conf
